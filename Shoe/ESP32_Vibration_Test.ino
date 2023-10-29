9cc63b6be7fe51c6892a4332acb0efdd235e2610
Cayden K <134308637+CaydenKeene@users.noreply.github.com>
2023-10-29 01:40:41 -0400

    working row/col buzzer

--- Shoe/ESP32_Vibration_Test.ino
+++ Shoe/ESP32_Vibration_Test.ino
@@ -7,22 +7,18 @@ const char* ssid = "worldhq";
 const char* password = "iwillforeverbetheboss";   
 const char* hostname = "esp32-left";
 
+const int vibrate = 10;
+
 WiFiServer server(80);
 
 void setup()
 {
     Serial.begin(115200);
     delay(10);
-
     WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
     MDNS.begin(hostname);
-
     server.begin();
+    pinMode(vibrate, OUTPUT);
 }
 
 void loop()
@@ -38,15 +34,47 @@ void loop()
                         client.println("HTTP/1.1 200 OK");
                         client.println("Content-type:text/html");
                         client.println();
-                        client.println("Hello from ESP32!");
+                        client.println("row and column received from esp32-left");
                         
-                        // Read data from the request
+                         // Read data from the request
+                        String sensor_data;
                         while(client.available()){
                             String line = client.readStringUntil('\r');
                             if (line.startsWith("sensor_data=")) {
-                                Serial.println(line.substring(12)); // Assuming data starts after "sensor_data="
+                                sensor_data = line.substring(12); // Assuming data starts after "sensor_data="
+                                break;
                             }
                         }
+
+                        // Split the string by '+'
+                        int plusIndex = sensor_data.indexOf('+');
+                        if (plusIndex != -1) {
+                            String value1 = sensor_data.substring(0, plusIndex);
+                            String value2 = sensor_data.substring(plusIndex + 1);
+
+                            int intValue1 = value1.toInt();
+                            int intValue2 = value2.toInt();
+
+                            Serial.print("Row: ");
+                            Serial.println(intValue1);
+                            Serial.print("Column: ");
+                            Serial.println(intValue2);
+
+                            for (int i = 0; i < intValue1; i++) {
+                              digitalWrite(vibrate, HIGH);
+                              delay(250);
+                              digitalWrite(vibrate, LOW);
+                              delay(250);
+                            }
+                            delay(1000);
+                            for (int i = 0; i < intValue2; i++) {
+                              digitalWrite(vibrate, HIGH);
+                              delay(250);
+                              digitalWrite(vibrate, LOW);
+                              delay(250);
+                            }
+                        }
+
                         break;
                     } else {
                         currentLine = "";
--- Shoe/vibration_test.py
+++ Shoe/vibration_test.py
@@ -3,8 +3,19 @@ import requests
 esp32_hostname = "esp32-left.local"  # Replace with your actual mDNS hostname
 url = f"http://{esp32_hostname}/endpoint"  # Define the URL of your ESP32 endpoint
 
-data = {'sensor_data': '69696969'}  # Replace with your actual data
+while True:
+    row = input("Enter row: ")
+    if row.lower() == 'exit':
+        break
+    # row_data = {'sensor_data': row}
+    col = input("Enter column: ")
+    if col.lower() == 'exit':
+        break
+    # col_data = {'sensor_data': col}
+    row_col = f'{row} {col}'
+    send_data = {'sensor_data': row_col}
+    response = requests.post(url, data=send_data)
+    # response = requests.post(url, data=row_data)
+    # response = requests.post(url, data=col_data)
 
-response = requests.post(url, data=data)
-
-print(response.text)
+    print(response.text)
