3fd4e504ec4dc54dc9256cb93d45660b72c58222
Cayden K <134308637+CaydenKeene@users.noreply.github.com>
2023-11-03 23:08:12 -0400

    added piece input

--- Shoe/vibration_test.py
+++ Shoe/vibration_test.py
@@ -4,6 +4,9 @@ esp32_hostname = "esp32-shoe.local"  # Replace with your actual mDNS hostname
 url = f"http://{esp32_hostname}/endpoint"  # Define the URL of your ESP32 endpoint
 
 while True:
+    piece = input("Enter piece number: ")
+    if piece.lower() == 'exit':
+        break
     row = input("Enter row: ")
     if row.lower() == 'exit':
         break
@@ -12,10 +15,10 @@ while True:
     if col.lower() == 'exit':
         break
     # col_data = {'sensor_data': col}
-    row_col = f'{row} {col}'
-    send_data = {'sensor_data': row_col}
+    piece_row_col = f'{piece} {row} {col}'
+    send_data = {'sensor_data': piece_row_col}
     response = requests.post(url, data=send_data)
     # response = requests.post(url, data=row_data)
     # response = requests.post(url, data=col_data)
-
+    
     print(response.text)
